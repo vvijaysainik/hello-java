c209a860b9fa3494e394a49fffadabbfb77890bb
vvijaysainik <vvijaysainik@gmail.com>
2023-07-13 08:09:24 -0400

    from eclipse

---
+++ hello/src/hello/Simple.java
@@ -0,0 +1,9 @@
+package hello;
+
+// This is the first git project in eclipse
+
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
